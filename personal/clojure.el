0d3e58204fe686021d8b133286ca48f48da6e15a
James Peterson <james@totaltechshop.com>
2014-02-21 10:41:37 -0700

    let's try smartparens

--- personal/clojure.el
+++ personal/clojure.el
@@ -1,6 +1,9 @@
 ;; Clojure related stuff
 ;;
 
+;; Let's try smartparens
+(prelude-require-package 'smartparens)
+
 ;; Cider
 (setq nrepl-hide-special-buffers t)
 (setq cider-repl-result-prefix ";; => ")
@@ -8,5 +11,6 @@
 (setq cider-repl-wrap-history t)
 (setq cider-repl-history-file "~/.emacs.d/cider-history")
 (add-hook 'cider-repl-mode-hook 'subword-mode) ; CamelCase support
-(add-hook 'cider-repl-mode-hook 'paredit-mode)
+;; (add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
