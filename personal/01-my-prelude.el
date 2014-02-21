010ad2c1dca5aac12dbe679cfcce945d51a8474b
James Peterson <james@totaltechshop.com>
2014-02-21 11:34:22 -0700

    add cyberpunk theme and other packages

--- personal/01-my-prelude.el
+++ personal/01-my-prelude.el
@@ -1,5 +1,8 @@
 ;; general prelude customisations
 
+;; additional packages
+(prelude-require-packages '(ag haml-mode js3-mode mustache-mode cyberpunk-theme))
+
 ;; aspell lives here
 (setq-default ispell-program-name "/usr/local/bin/aspell")
 
